a4ea06af8ec2dc807b3823be215a2427807b9ce2
giovane <giovane.sobrinho@gmail.com>
2019-11-12 15:34:35 -0300

    update

--- API2/pontogrpg/start/routes.js
+++ API2/pontogrpg/start/routes.js
@@ -16,6 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/register', 'AuthController.register')
+Route.post('/authenticate', 'AuthController.authenticate')
