b7e62393908a8549aae3a9a836d466fcf8e7bcec
Guilherme Abranches <guilherme.abranches@outlook.com>
2019-11-13 19:51:17 -0300

    Atributos  funcionando com JS
    E muitas outras modificações

---
+++ template_Projeto_Final/game-warrior-gh-pages/js/func_Ficha.js
@@ -0,0 +1,198 @@
+document.getElementById("agilidade").addEventListener("input", function() {
+
+    if (document.getElementById("agilidade").value <= 11) {
+        document.getElementById("inputGroup-sizing").innerHTML = "+0"
+    } else {
+        if (document.getElementById("agilidade").value == 12 || document.getElementById("agilidade").value == 13) {
+            document.getElementById("inputGroup-sizing").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("agilidade").value == 14 || document.getElementById("agilidade").value == 15) {
+                document.getElementById("inputGroup-sizing").innerHTML = "+2"
+            } else {
+                if (document.getElementById("agilidade").value == 16 || document.getElementById("agilidade").value == 17) {
+                    document.getElementById("inputGroup-sizing").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("agilidade").value == 18 || document.getElementById("agilidade").value == 19) {
+                        document.getElementById("inputGroup-sizing").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("agilidade").value == 20) {
+                            document.getElementById("inputGroup-sizing").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
+document.getElementById("carisma").addEventListener("input", function() {
+
+    if (document.getElementById("carisma").value <= 11) {
+        document.getElementById("inputGroup-sizing1").innerHTML = "+0"
+    } else {
+        if (document.getElementById("carisma").value == 12 || document.getElementById("carisma").value == 13) {
+            document.getElementById("inputGroup-sizing1").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("carisma").value == 14 || document.getElementById("carisma").value == 15) {
+                document.getElementById("inputGroup-sizing1").innerHTML = "+2"
+            } else {
+                if (document.getElementById("carisma").value == 16 || document.getElementById("carisma").value == 17) {
+                    document.getElementById("inputGroup-sizing1").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("carisma").value == 18 || document.getElementById("carisma").value == 19) {
+                        document.getElementById("inputGroup-sizing1").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("carisma").value == 20) {
+                            document.getElementById("inputGroup-sizing1").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
+document.getElementById("constituicao").addEventListener("input", function() {
+
+    if (document.getElementById("constituicao").value <= 11) {
+        document.getElementById("inputGroup-sizing2").innerHTML = "+0"
+    } else {
+        if (document.getElementById("constituicao").value == 12 || document.getElementById("constituicao").value == 13) {
+            document.getElementById("inputGroup-sizing2").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("constituicao").value == 14 || document.getElementById("constituicao").value == 15) {
+                document.getElementById("inputGroup-sizing2").innerHTML = "+2"
+            } else {
+                if (document.getElementById("constituicao").value == 16 || document.getElementById("constituicao").value == 17) {
+                    document.getElementById("inputGroup-sizing2").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("constituicao").value == 18 || document.getElementById("constituicao").value == 19) {
+                        document.getElementById("inputGroup-sizing2").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("constituicao").value == 20) {
+                            document.getElementById("inputGroup-sizing2").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
+document.getElementById("destreza").addEventListener("input", function() {
+
+    if (document.getElementById("destreza").value <= 11) {
+        document.getElementById("inputGroup-sizing3").innerHTML = "+0"
+    } else {
+        if (document.getElementById("destreza").value == 12 || document.getElementById("destreza").value == 13) {
+            document.getElementById("inputGroup-sizing3").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("destreza").value == 14 || document.getElementById("destreza").value == 15) {
+                document.getElementById("inputGroup-sizing3").innerHTML = "+2"
+            } else {
+                if (document.getElementById("destreza").value == 16 || document.getElementById("destreza").value == 17) {
+                    document.getElementById("inputGroup-sizing3").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("destreza").value == 18 || document.getElementById("destreza").value == 19) {
+                        document.getElementById("inputGroup-sizing3").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("destreza").value == 20) {
+                            document.getElementById("inputGroup-sizing3").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
+document.getElementById("forca").addEventListener("input", function() {
+
+    if (document.getElementById("forca").value <= 11) {
+        document.getElementById("inputGroup-sizing4").innerHTML = "+0"
+    } else {
+        if (document.getElementById("forca").value == 12 || document.getElementById("forca").value == 13) {
+            document.getElementById("inputGroup-sizing4").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("forca").value == 14 || document.getElementById("forca").value == 15) {
+                document.getElementById("inputGroup-sizing4").innerHTML = "+2"
+            } else {
+                if (document.getElementById("forca").value == 16 || document.getElementById("forca").value == 17) {
+                    document.getElementById("inputGroup-sizing4").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("forca").value == 18 || document.getElementById("forca").value == 19) {
+                        document.getElementById("inputGroup-sizing4").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("forca").value == 20) {
+                            document.getElementById("inputGroup-sizing4").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
+document.getElementById("inteligencia").addEventListener("input", function() {
+
+    if (document.getElementById("inteligencia").value <= 11) {
+        document.getElementById("inputGroup-sizing5").innerHTML = "+0"
+    } else {
+        if (document.getElementById("inteligencia").value == 12 || document.getElementById("inteligencia").value == 13) {
+            document.getElementById("inputGroup-sizing5").innerHTML = "+1"
+
+        } else {
+            if (document.getElementById("inteligencia").value == 14 || document.getElementById("inteligencia").value == 15) {
+                document.getElementById("inputGroup-sizing5").innerHTML = "+2"
+            } else {
+                if (document.getElementById("inteligencia").value == 16 || document.getElementById("inteligencia").value == 17) {
+                    document.getElementById("inputGroup-sizing5").innerHTML = "+3"
+                } else {
+                    if (document.getElementById("inteligencia").value == 18 || document.getElementById("inteligencia").value == 19) {
+                        document.getElementById("inputGroup-sizing5").innerHTML = "+4"
+                    } else {
+                        if (document.getElementById("inteligencia").value == 20) {
+                            document.getElementById("inputGroup-sizing5").innerHTML = "+5"
+                        } else {
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+});
